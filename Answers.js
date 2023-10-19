6272758e22c54320e17f5f5467db4c89fd38fab3
Oueael <125812553+Oueael@users.noreply.github.com>
2023-10-18 21:58:24 -0400

    Update Answers.js

--- Answers.js
+++ Answers.js
@@ -43,3 +43,37 @@ function countSheeps(sheep) {
   return sheep.filter(s => s === true).length;
 }
 =======================================================
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+Example
+
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+    // Check if input is null or empty
+    if (input === null || input.length === 0) {
+        return [];
+    }
+
+    let positiveCount = 0;
+    let negativeSum = 0;
+
+    // Loop through the input array
+    for(let i = 0; i < input.length; i++) {
+        // Check if the current number is positive
+        if(input[i] > 0) {
+            positiveCount++;
+        } 
+        // Check if the current number is negative
+        else if(input[i] < 0) {
+            negativeSum += input[i];
+        }
+    }
+
+    // Return the results in a new array
+    return [positiveCount, negativeSum];
+}
+=========================================================
